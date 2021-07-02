fd3b8154ff0058636d3e5c35f6229be0f0eed85f
root <root@ubuntu.ubuntu-domain>
2021-07-02 13:18:59 -0300

    fixed prompt message

--- src/prompt.js
+++ src/prompt.js
@@ -1,27 +1,27 @@
-const prompt = require('prompt');
-const actualDir = require('./actualDir')
+const prompt = require("prompt");
+const actualDir = require("./actualDir");
 let message_ = [];
 
-//shows '...'  instead complete path
-if (actualDir.path.length > 2)
-    message_ = actualDir.path.map((dir) =>
-        actualDir.path.indexOf(dir) == 0 ||
-        actualDir.path.indexOf(dir) == actualDir.path.length - 1
-            ? dir
-            : actualDir.path.indexOf(dir) == 1
-            ? ".../"
-            : ""
-    );
-else {
-    message_ = actualDir.path;
-}
-
 module.exports = {
-    input : async () =>{
+    input: async () => {
+        //shows '...'  instead complete path
+        if (actualDir.path.length > 2) {
+            message_ = actualDir.path.map((dir) =>
+                actualDir.path.indexOf(dir) === 0 ||
+                actualDir.path.indexOf(dir) === actualDir.path.length - 1
+                    ? dir
+                    : actualDir.path.indexOf(dir) === 1
+                    ? ".../"
+                    : ""
+            );
+        } else {
+            message_ = actualDir.path;
+        }
+
         prompt.message = message_.join("");
         prompt.delimiter = "";
         prompt.start();
-        const receivedInput = await prompt.get(">")
+        const receivedInput = await prompt.get(">");
         return receivedInput[">"];
-}
-}
+    },
+};
